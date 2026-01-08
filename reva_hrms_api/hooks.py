7f15d240c32ef2f77fc49944962ab73a97f44c17
Abhishek Wadhai <abhishek.wadhai@excellminds.com>
2026-01-08 15:32:54 +0530

    update fixture for otp field and expire field

--- reva_hrms_api/hooks.py
+++ reva_hrms_api/hooks.py
@@ -242,3 +242,16 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+
+
+fixtures = [
+    {
+        "dt": "Custom Field",
+        "filters": [
+            ["dt", "=", "User"],
+            ["fieldname", "in", ["otp", "otp_expire_time"]]
+        ]
+    }
+]
+
+
